578a69f9f59a5edb6f16ea326f3c6bbff0d3bf91
findk <findkh@nate.com>
2024-01-05 20:57:01 +0900

    24.01.05. 단방향 연관관계

--- JPA_study/ch5-jpa-study/src/main/java/jpabook/model/JpaMain.java
+++ JPA_study/ch5-jpa-study/src/main/java/jpabook/model/JpaMain.java
@@ -1,6 +1,8 @@
 package jpabook.model;
 
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -21,6 +23,9 @@ public class JpaMain {
 			tx.begin(); //트랜잭션 시작
 			
 			testSave(em);
+			queryLogicJoin(em);
+			updateReloation(em);
+			deleteRelation(em);
 			
 			tx.commit();//트랜잭션 커밋
 		} catch (Exception e) {
@@ -32,6 +37,38 @@ public class JpaMain {
 		emf.close(); //엔티티 매니저 팩토리 종료
 	}
 	
+	//연관관계 제거
+	private static void deleteRelation(EntityManager em) {
+		Member member1 = em.find(Member.class, "member1");
+		member1.setTeam(null); //연관관계 제거
+		
+	}
+	
+	
+	//수정
+	private static void updateReloation(EntityManager em) {
+		Team team2 = new Team("Team2", "팀2");
+		em.persist(team2);
+		
+		Member member = em.find(Member.class, "member1");
+		member.setTeam(team2);
+	}
+	
+	
+	//조회
+	public static void queryLogicJoin(EntityManager em) {
+		String jpql = "SELECT m FROM Member m JOIN m.team t WHERE t.name=:teamName";
+		
+		List<Member> resultList = em.createQuery(jpql, Member.class)
+				.setParameter("teamName", "팀1")
+				.getResultList();
+		
+		for(Member member : resultList) {
+			System.out.println("[query] member.username=" + member.getUsername());
+		}
+	}
+	
+	//저장
 	public static void testSave(EntityManager em) {
 		//팀1 저장
 		Team team1 = new Team("Team1", "팀1");
@@ -47,6 +84,4 @@ public class JpaMain {
 		member2.setTeam(team1);
 		em.persist(member2);
 	}
-
-
 }
