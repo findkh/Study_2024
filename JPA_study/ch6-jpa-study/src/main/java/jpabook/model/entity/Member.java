ef97462d80370e1815f3e81415e8e0f8a70701a4
findk <findkh@nate.com>
2024-01-08 21:24:39 +0900

    24.01.08.다대일 단방향[N:1]

---
+++ JPA_study/ch6-jpa-study/src/main/java/jpabook/model/entity/Member.java
@@ -0,0 +1,50 @@
+package jpabook.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Member {
+
+	@Id
+	@Column(name = "member_id")
+	private String id;
+	
+	private String username;
+	
+	@ManyToOne
+	@JoinColumn(name = "team_id")
+	private Team team;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", username=" + username + ", team=" + team + "]";
+	}
+}
---
+++ JPA_study/ch6-jpa-study/src/main/java/jpabook/model/entity/Team.java
@@ -0,0 +1,37 @@
+package jpabook.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+	@Id
+	@Column(name = "team_id")
+	private String id;
+	
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Team [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+}
