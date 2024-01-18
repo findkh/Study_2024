1d9b2df7c9508c9927ab2cf4bf424dfbb7191c3c
findk <findkh@nate.com>
2024-01-18 19:53:07 +0900

    24.01.18. 다양한 연관관계 매핑

--- JPA_study/ch6-jpa-study/src/main/java/jpabook/model/JpaMain.java
+++ JPA_study/ch6-jpa-study/src/main/java/jpabook/model/JpaMain.java
@@ -1,25 +1,20 @@
 package jpabook.model;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import jpabook.model.entity.Member;
-import jpabook.model.entity.MemberProduct;
-import jpabook.model.entity.MemberProductId;
+import jpabook.model.entity.Order;
 import jpabook.model.entity.Product;
 
 public class JpaMain {
 	
-	/* 다대다[N:N]
-	 * -> 관계형 데이터베이스는 정규화된 테이블 2개로 다대다 관계를 표현할 수 없다.
-	 * 그래서 중간에 연결 테이블을 추가해야 한다. 
-	 * 하지만 객체는 테이블과 다르게 객체 2개로 다대다 관계를 만들 수 있다.
-	 * @ManytoMany를 사용하면 매핑할 수 있다.
-	 * 
+	/* 다대다 연관관계
+	 *  다대다 관계를 일대다 다대일 관계로 풀어내기 위해 연결 테이블을 만들 때 식별자를 어떻게 구성할지 선택해야한다.
+	 *  식별 관계: 받아온 식별자를 기본 키 + 외래 키로 사용
+	 *  비식별 관계: 받아온 식별자는 외래 키로만 사용하고 새로운 식별자를 추가한다.
 	 * */
 	
 	public static void main(String[] args) {
@@ -31,10 +26,8 @@ public class JpaMain {
 
 		try {
 			tx.begin(); //트랜잭션 시작
-			testSave2(em);
-			find2(em);
-//			testSave(em);
-//			find(em);
+			testSave(em);
+			testFind(em);
 			tx.commit();//트랜잭션 커밋
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -45,10 +38,10 @@ public class JpaMain {
 		emf.close(); //엔티티 매니저 팩토리 종료
 	}
 	
-	public static void testSave2(EntityManager em) {
+	public static void testSave(EntityManager em) {
 		Member member1 = new Member();
 			member1.setId("member1");
-			member1.setUsername("회원1");
+			member1.setUserName("회원1");
 		em.persist(member1);
 		
 		Product productA = new Product();
@@ -56,46 +49,23 @@ public class JpaMain {
 			productA.setName("상품1");
 		em.persist(productA);
 		
-		MemberProduct memberProduct = new MemberProduct();
-		memberProduct.setMember(member1);
-		memberProduct.setProduct(productA);
-		memberProduct.setOrderAmout(3);
-		em.persist(memberProduct);
+		Order order = new Order();
+			order.setMember(member1);
+			order.setProduct(productA);
+			order.setOrderAmount(2);
+		em.persist(order);
 	}
 	
-	public static void find2(EntityManager em) {
-		MemberProductId memberProductId = new MemberProductId();
-		memberProductId.setMember("member1");
-		memberProductId.setProduct("productA");
+	public static void testFind(EntityManager em) {
+		Long orderId = 1L;
+		Order order = em.find(Order.class, orderId);
 		
-		MemberProduct memberProduct = em.find(MemberProduct.class, memberProductId);
 		
-		Member member = memberProduct.getMember();
-		Product product = memberProduct.getProduct();
+		Member member = order.getMember();
+		Product product = order.getProduct();
 		
-		System.out.println("member = " + member.getUsername());
+		System.out.println("member = " + member.getUserName());
 		System.out.println("product = " + product.getName());
-		System.out.println("orderAmount = " + memberProduct.getOrderAmout());
+		System.out.println("orderAmount = " + order.getOrderAmount());
 	}
-	
-//	public static void testSave(EntityManager em) {
-//		Product productA = new Product();
-//		productA.setId("productA");
-//		productA.setName("상품A");
-//		em.persist(productA);
-//		
-//		Member member1 = new Member();
-//			member1.setId("member1");
-//			member1.setUsername("회원1");
-//			member1.getProducts().add(productA);
-//		em.persist(member1);
-//	}
-//	
-//	public static void find(EntityManager em) {
-//		Member member = em.find(Member.class, "member1");
-//		List<Product> products = member.getProducts(); //객체 그래프 탐색
-//		for(Product product : products) {
-//			System.out.println("product.name = " + product.getName());
-//		}
-//	}
 }
--- JPA_study/ch6-jpa-study/src/main/java/jpabook/model/entity/Member.java
+++ JPA_study/ch6-jpa-study/src/main/java/jpabook/model/entity/Member.java
@@ -13,21 +13,13 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class Member {
-
-	//다대다 양방향
-	@Id @Column(name = "member_id")
+	@Id @Column(name = "member")
 	private String id;
-
-	private String username;
 	
-	//역방향
+	private String userName;
+	
 	@OneToMany(mappedBy = "member")
-	private List<MemberProduct> memberProducts;
-
-	@Override
-	public String toString() {
-		return "Member [id=" + id + ", memberProducts=" + memberProducts + "]";
-	}
+	private List<Order> orders = new ArrayList<Order>();
 
 	public String getId() {
 		return id;
@@ -37,142 +29,25 @@ public class Member {
 		this.id = id;
 	}
 
-	public String getUsername() {
-		return username;
+	public String getUserName() {
+		return userName;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 
-	public List<MemberProduct> getMemberProducts() {
-		return memberProducts;
+	public List<Order> getOrders() {
+		return orders;
 	}
 
-	public void setMemberProducts(List<MemberProduct> memberProducts) {
-		this.memberProducts = memberProducts;
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", userName=" + userName + ", orders=" + orders + "]";
+	}
 	
-	
-	
-	
-//	//다대다 단방향
-//	@Id @Column(name = "member_id")
-//	private String id;
-//	
-//	private String username;
-//	
-//	@ManyToMany
-//	@JoinTable(name = "member_product", joinColumns = @JoinColumn(name = "member_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
-//	private List<Product> products = new ArrayList<Product>();
-//
-//	public String getId() {
-//		return id;
-//	}
-//
-//	public void setId(String id) {
-//		this.id = id;
-//	}
-//
-//	public String getUsername() {
-//		return username;
-//	}
-//
-//	public void setUsername(String username) {
-//		this.username = username;
-//	}
-//
-//	public List<Product> getProducts() {
-//		return products;
-//	}
-//
-//	public void setProducts(List<Product> products) {
-//		this.products = products;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Member [id=" + id + ", username=" + username + ", products=" + products + "]";
-//	}
-//	
-//	
-//	
-	
-	
-//	@Id @GeneratedValue
-//	@Column(name = "member_id")
-//	private Long id;
-//	
-//	private String username;
-//	
-//	@OneToOne
-//	@JoinColumn(name = "locker_id")
-//	private Locker locker;
-//
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	public String getUsername() {
-//		return username;
-//	}
-//
-//	public void setUsername(String username) {
-//		this.username = username;
-//	}
-//
-//	public Locker getLocker() {
-//		return locker;
-//	}
-//
-//	public void setLocker(Locker locker) {
-//		this.locker = locker;
-//	}
-//	
-	
-	
-//	@Id
-//	@GeneratedValue
-//	@Column(name = "member_id")
-//	private Long id;
-//	
-//	private String username;
-//
-//	@ManyToOne
-//	@JoinColumn(name = "team_id", insertable = false, updatable = false)
-//	private Team team;
-//
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	public String getUsername() {
-//		return username;
-//	}
-//
-//	public void setUsername(String username) {
-//		this.username = username;
-//	}
-//
-//	public Team getTeam() {
-//		return team;
-//	}
-//
-//	public void setTeam(Team team) {
-//		this.team = team;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Member [id=" + id + ", username=" + username + ", team=" + team + "]";
-//	}
 }
