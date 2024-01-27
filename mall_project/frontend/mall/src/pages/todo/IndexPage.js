65055b61c95bc6a20ad9ab06df188656d308a130
findk <findkh@nate.com>
2024-01-27 17:24:16 +0900

    24.01.27. react 조회, 페이징처리

--- mall_project/frontend/mall/src/pages/todo/IndexPage.js
+++ mall_project/frontend/mall/src/pages/todo/IndexPage.js
@@ -13,20 +13,22 @@ const IndexPage = () => {
 
   return (
     <BasicLayout>
-      <div className="w-full flex m-2 p-2">
+      <div className="w-full flex m-2 p-2 ">
         <div
-          className="tet-xl m-1 p-2 w-20 font-extrabold text-center underline"
+          className="text-xl m-1 p-2  w-20 font-extrabold text-center underline"
           onClick={handleClickList}
         >
           LIST
         </div>
+
         <div
-          className="tet-xl m-1 p-2 w-20 font-extrabold text-center underline"
+          className="text-xl m-1 p-2 w-20 font-extrabold  text-center underline"
           onClick={handleClickAdd}
         >
           ADD
         </div>
-        <div className="flex flex-wrap w-full"></div>
+      </div>
+      <div className="flex flex-wrap w-full">
         <Outlet />
       </div>
     </BasicLayout>
