6836fe10972662b8721805f5000557701dedc0b8
findk <findkh@nate.com>
2024-02-01 19:28:29 +0900

    24.02.01. 상품 API 서버구성

--- mall_project/backend/mallapi/src/main/java/com/kh/mallapi/controller/ProductController.java
+++ mall_project/backend/mallapi/src/main/java/com/kh/mallapi/controller/ProductController.java
@@ -2,13 +2,17 @@ package com.kh.mallapi.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.mallapi.dto.PageRequestDTO;
+import com.kh.mallapi.dto.PageResponseDTO;
 import com.kh.mallapi.dto.ProductDTO;
+import com.kh.mallapi.service.ProductService;
 import com.kh.mallapi.util.CustomFileUtil;
 
 import lombok.RequiredArgsConstructor;
@@ -16,8 +20,12 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable
+
+;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,16 +33,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("/api/products")
 public class ProductController {
     private final CustomFileUtil fileUtil;
+    private final ProductService productService;
 
     @PostMapping("/")
-    public Map<String, String> register(ProductDTO productDTO) {
+    public Map<String, Long> register(ProductDTO productDTO) {
         log.info("register: {}", productDTO);
 
         List<MultipartFile> files = productDTO.getFiles();
         List<String> uploadFileNames = fileUtil.saveFiles(files);
         productDTO.setUploadFileNames(uploadFileNames);
         log.info(uploadFileNames);
-        return Map.of("RESULT", "SUCCESS");
+
+        Long pno = productService.register(productDTO);
+
+        return Map.of("result", pno);
     }
 
     @GetMapping("/view/{fileName}")
@@ -44,4 +56,47 @@ public class ProductController {
         return fileUtil.getFile(fileName);
     }
 
+    @GetMapping("/list")
+    public PageResponseDTO<ProductDTO> list(PageRequestDTO pageRequestDTO) {
+        log.info("list: {}", pageRequestDTO);
+        return productService.getList(pageRequestDTO);
+    }
+
+    @GetMapping("/{pno}")
+    public ProductDTO read(@PathVariable(name = "pno") Long pno) {
+        return productService.get(pno);
+    }
+
+    @PutMapping("/{pno}")
+    public Map<String, String> modify(@PathVariable(name = "pno") Long pno, ProductDTO productDTO) {
+        productDTO.setPno(pno);
+        ProductDTO oldProductDTO = productService.get(pno);
+        List<String> oldFileNames = oldProductDTO.getUploadFileNames();
+        List<MultipartFile> files = productDTO.getFiles();
+        List<String> currentUploadFileNames = fileUtil.saveFiles(files);
+        List<String> uploaddedFileNames = productDTO.getUploadFileNames();
+
+        if (currentUploadFileNames != null && currentUploadFileNames.size() > 0) {
+            uploaddedFileNames.addAll(currentUploadFileNames);
+        }
+
+        productService.modify(productDTO);
+
+        if (oldFileNames != null && oldFileNames.size() > 0) {
+            List<String> removeFiles = oldFileNames.stream()
+                    .filter(fileName -> uploaddedFileNames.indexOf(fileName) == -1).collect(Collectors.toList());
+            fileUtil.deleteFiles(removeFiles);
+        }
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{pno}")
+    public Map<String, String> remove(@PathVariable("pno") Long pno) {
+        List<String> oldFileNames = productService.get(pno).getUploadFileNames();
+        System.out.println("이전 파일=====ㄴ");
+        System.out.println(oldFileNames);
+        productService.remove(pno);
+        fileUtil.deleteFiles(oldFileNames);
+        return Map.of("RESULT", "SUCCESS");
+    }
 }
