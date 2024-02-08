8f8e2ac7c10a5e5fd8dd6b323eb8ce5a3746fecc
findk <findkh@nate.com>
2024-02-08 15:48:57 +0900

    24.02.08. 스프링 시큐리티 설정

--- mall_project/backend/mallapi/src/main/java/com/kh/mallapi/config/CustomServletConfig.java
+++ mall_project/backend/mallapi/src/main/java/com/kh/mallapi/config/CustomServletConfig.java
@@ -2,7 +2,6 @@ package com.kh.mallapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.kh.mallapi.controller.formatter.LocalDateFormatter;
@@ -16,14 +15,17 @@ public class CustomServletConfig implements WebMvcConfigurer {
         registry.addFormatter(new LocalDateFormatter());
     }
 
+    // Spring Security 설정 전 CORS 설정
     // CORS (Cross-Origin Resource Sharing) 설정을 추가하는 메서드
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // 모든 오리진 허용
-                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용하는 HTTP 메서드 지정
-                .maxAge(300) // Preflight 요청 결과를 캐시하는 시간 (초 단위)
-                .allowedHeaders("Authorization", "Cache-Control", "Content-Type"); // 허용하는 헤더 지정
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    // registry.addMapping("/**")
+    // .allowedOrigins("*") // 모든 오리진 허용
+    // .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용하는
+    // HTTP 메서드 지정
+    // .maxAge(300) // Preflight 요청 결과를 캐시하는 시간 (초 단위)
+    // .allowedHeaders("Authorization", "Cache-Control", "Content-Type"); // 허용하는 헤더
+    // 지정
+    // }
 
 }
