56158a44bcb7525adda8d9e6b65e9bf87608aee7
findk <findkh@nate.com>
2024-07-12 15:52:22 +0900

    2024.07.12. 팩토리 패턴 연습

---
+++ GoF/factory/src/factory_pizza/PizzaStore.java
@@ -0,0 +1,17 @@
+package factory_pizza;
+
+public abstract class PizzaStore {
+	
+	public Pizza orderPizza(String type) {
+		Pizza pizza = createPizza(type);
+		
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+		
+		return pizza;
+	}
+	
+	protected abstract Pizza createPizza(String type); //팩토리 메소드를 추상 메소드로 선언해서 서브 클래스가 객체 생성을 하도록 한다.
+}
