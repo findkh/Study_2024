be4b1a92ff7208d467433f144d39ac178b261728
findkh <findkh@nate.com>
2024-09-21 12:56:54 +0900

    2024.09.21. 리액트 강의

---
+++ React/section08/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState, useRef } from "react";
+import "./App.css";
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "출근하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "놀러 가기",
+    date: new Date().getTime(),
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState([mockData]);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDOne: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List />
+    </div>
+  );
+}
+
+export default App;
